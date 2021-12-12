818a742b8f8a5a89bdd74ebd378e39dd489baf63
Izan Blanes Aracil <izanblanesaracil@gmail.com>
2021-12-12 20:09:14 +0100

    "[NEW] Primer commit - nuevo proyecto Java" "[NEW] Segundo commit - cambio nuevo projecto Java" "[NEW] Tercer commit Subida cambios Ramas"

--- src/NumerosEnteros.java
+++ src/NumerosEnteros.java
@@ -1,6 +1,6 @@
 public class NumerosEnteros {
     public static void main(String[] args) {
-        for (int i = 1; i <= 20; i++) {
+        for (int i = 1; i <= 25; i++) {
             System.out.println(i);
         }
     }
